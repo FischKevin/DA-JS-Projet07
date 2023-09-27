b8ff7734fc5c34f3a330413a359ca19440fe4dbd
Kévin Fischer <kevinfischer@live.fr>
2023-09-27 16:44:59 +0200

    :sparkles: added update of recipes number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,18 @@
 import { recipes } from './recipes.js';
 import { recipesFactory } from './factories/recipeFactory.js';
+
+// Display recipes number
+// Select .recipesNumber element
+const recipesNumberElement = document.querySelector('.recipesNumber');
+
+// Select all .recipe elements in .recipeSection
+const allRecipes = document.querySelectorAll('.recipeSection .recipe');
+
+// Update .recipesNumber element with the number of recipes
+if (allRecipes.length === 0) {
+  recipesNumberElement.textContent = '0 recette';
+} else if (allRecipes.length === 1) {
+  recipesNumberElement.textContent = '1 recette';
+} else {
+  recipesNumberElement.textContent = allRecipes.length + ' recettes';
+}
