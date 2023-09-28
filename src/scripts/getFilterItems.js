436d536b0d8cf778f62ebe8aba12c092a3e391b8
Kévin Fischer <kevinfischer@live.fr>
2023-09-28 17:28:08 +0200

    :sparkles: sort a to z and capitalize first letters for dropdown li element

--- src/scripts/getFilterItems.js
+++ src/scripts/getFilterItems.js
@@ -1,5 +1,9 @@
 import { recipes } from './recipes.js';
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function populateDropdown(ulSelector, itemsSet) {
   const ulElement = document.querySelector(ulSelector);
   ulElement.innerHTML = '';
@@ -19,6 +23,12 @@ export function getIngredientsOptions() {
     });
   });
 
+  ingredientsSet = new Set(
+    [...ingredientsSet]
+      .map(capitalizeFirstLetter)
+      .sort((a, b) => a.localeCompare(b, 'fr')),
+  );
+
   populateDropdown(
     '#dropdownSearchIngredients + .fa-solid + ul',
     ingredientsSet,
@@ -27,20 +37,34 @@ export function getIngredientsOptions() {
 
 export function getApparelsOptions() {
   let appliancesSet = new Set();
+
   recipes.forEach((recipe) => {
     appliancesSet.add(recipe.appliance);
   });
+
+  appliancesSet = new Set(
+    [...appliancesSet]
+      .map(capitalizeFirstLetter)
+      .sort((a, b) => a.localeCompare(b, 'fr')),
+  );
 
   populateDropdown('#dropdownSearchAppareils + .fa-solid + ul', appliancesSet);
 }
 
 export function getUstensilsOptions() {
   let ustensilsSet = new Set();
+
   recipes.forEach((recipe) => {
     recipe.ustensils.forEach((ustensil) => {
       ustensilsSet.add(ustensil);
     });
   });
+
+  ustensilsSet = new Set(
+    [...ustensilsSet]
+      .map(capitalizeFirstLetter)
+      .sort((a, b) => a.localeCompare(b, 'fr')),
+  );
 
   populateDropdown('#dropdownSearchUstensiles + .fa-solid + ul', ustensilsSet);
 }
