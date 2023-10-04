3911cc172fe0e83c47669c8f70357b18d7333060
Kévin Fischer <kevinfischer@live.fr>
2023-10-04 18:04:29 +0200

    :sparkles: added tag and li management when a tag is already selected

--- src/scripts/handleTags.js
+++ src/scripts/handleTags.js
@@ -1,19 +1,37 @@
+// JavaScript
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.dropdownContent ul li').forEach((item) => {
     item.addEventListener('click', () => {
-      addTag(item.textContent);
+      if (!item.classList.contains('selected')) {
+        addTag(item.textContent);
+        item.classList.add('selected');
+      }
     });
   });
 });
 
+const addedTags = new Set();
+
 export function addTag(content) {
+  if (addedTags.has(content)) {
+    return;
+  }
+
   const tagsContainer = document.querySelector('.addedTags');
   const newTag = document.createElement('div');
   newTag.className = 'tag';
   newTag.textContent = content;
+  addedTags.add(content);
 
   newTag.addEventListener('click', () => {
     newTag.remove();
+    addedTags.delete(content);
+
+    document.querySelectorAll('.dropdownContent ul li').forEach((item) => {
+      if (item.textContent === content) {
+        item.classList.remove('selected');
+      }
+    });
   });
 
   tagsContainer.appendChild(newTag);
