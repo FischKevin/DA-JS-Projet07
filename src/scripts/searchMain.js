07655bf6143b756456c45b27e6defc592c41f385
Kévin Fischer <kevinfischer@live.fr>
2023-10-19 11:43:48 +0200

    :bulb: added comment to searchMain.js

--- src/scripts/searchMain.js
+++ src/scripts/searchMain.js
@@ -5,6 +5,7 @@ import { updateListOptions } from './handleDropDown.js';
 
 export let currentSearchQuery = '';
 
+// Handle search input
 document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.querySelector('.header--input');
 
@@ -12,7 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
     currentSearchQuery = searchInput.value.trim().toLowerCase();
     const matchedRecipes =
       currentSearchQuery.length >= 3
-        ? searchRecipes(currentSearchQuery)
+        ? // If the search query is at least 3 characters long, search the recipes, else display all recipes
+          searchRecipes(currentSearchQuery)
         : recipes;
     updateRecipeSection(matchedRecipes);
   });
@@ -22,34 +24,37 @@ document.addEventListener('DOMContentLoaded', () => {
 export function searchRecipes(query, recipesToFilter = recipes) {
   const matchedRecipes = [];
   const searchTerms = query.split(' ');
-
+  // For each recipe, check if all the search terms are found in the recipe name, description, or ingredients
   for (let i = 0; i < recipesToFilter.length; i++) {
     const { name, ingredients, description } = recipesToFilter[i];
 
     let searchableText = name.toLowerCase() + ' ' + description.toLowerCase();
-
+    // Add all the ingredients to the searchable text
     for (let j = 0; j < ingredients.length; j++) {
       searchableText += ' ' + ingredients[j].ingredient.toLowerCase();
     }
 
     let allTermsFound = true;
+    // Check if all the search terms are found in the searchable text
     for (let j = 0; j < searchTerms.length; j++) {
       const term = searchTerms[j].toLowerCase();
 
       let termFound = false;
+      // Check if the search term is found in the searchable text
       for (let k = 0; k <= searchableText.length - term.length; k++) {
+        // If the search term is found, break the loop
         if (searchableText.substring(k, k + term.length) === term) {
           termFound = true;
           break;
         }
       }
-
+      // If the search term is not found, break the loop
       if (!termFound) {
         allTermsFound = false;
         break;
       }
     }
-
+    // If all the search terms are found, add the recipe to the matched recipes
     if (allTermsFound) {
       matchedRecipes.push(recipesToFilter[i]);
     }
@@ -62,7 +67,7 @@ export function searchRecipes(query, recipesToFilter = recipes) {
 export function updateRecipeSection(matchedRecipes) {
   const recipeSection = document.querySelector('.recipeSection');
   recipeSection.textContent = '';
-
+  // If no recipe is found, display an error message
   if (matchedRecipes.length === 0) {
     const errorMessage = document.createElement('p');
     errorMessage.classList.add('errorMessage');
@@ -70,6 +75,7 @@ export function updateRecipeSection(matchedRecipes) {
       .querySelector('.header--input')
       .value.trim()}’ vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
     recipeSection.appendChild(errorMessage);
+    // Else, display the matched recipes
   } else {
     matchedRecipes.forEach((recipe) => {
       const recipeCard = recipesFactory(recipe);
