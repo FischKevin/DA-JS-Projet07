6957a17fe484b3945bf1996495e7d040943e517e
Kévin Fischer <kevinfischer@live.fr>
2023-10-31 13:06:16 +0100

    :green_heart: minified css

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,31 +1,18 @@
 export function updateRecipeCount() {
-  // Select .recipesNumber element
-  const recipesNumberElement = document.querySelector('.recipesNumber');
-
-  // Select all .recipe elements in .recipeSection
-  const allRecipes = document.querySelectorAll('.recipeSection .recipe');
-
-  // Update .recipesNumber element with the number of recipes
-  if (allRecipes.length === 0) {
-    recipesNumberElement.textContent = '0 recette';
-  } else if (allRecipes.length === 1) {
-    recipesNumberElement.textContent = '1 recette';
-  } else {
-    recipesNumberElement.textContent = allRecipes.length + ' recettes';
-  }
+  const e = document.querySelector('.recipesNumber'),
+    t = document.querySelectorAll('.recipeSection .recipe');
+  0 === t.length
+    ? (e.textContent = '0 recette')
+    : 1 === t.length
+    ? (e.textContent = '1 recette')
+    : (e.textContent = t.length + ' recettes');
 }
-
-// Clear search field
-export function clearSearchFields(inputIds) {
-  inputIds.forEach((id) => {
-    const inputElement = document.getElementById(id);
-    if (inputElement) {
-      inputElement.value = '';
-    }
+export function clearSearchFields(e) {
+  e.forEach((e) => {
+    const t = document.getElementById(e);
+    t && (t.value = '');
   });
 }
-
-// Capitalize first letter
-export function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+export function capitalizeFirstLetter(e) {
+  return e.charAt(0).toUpperCase() + e.slice(1);
 }
